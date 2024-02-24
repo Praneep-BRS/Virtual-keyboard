a6ac0607360c550904a9e10a35464d77ebbe7847
Praneep-BRS <praneep.prani@gmail.com>
2024-02-24 15:01:40 +0530

    implemented dynamic function for keys object

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,43 @@
 "use strict";
 
-const keys = [
-  { id: "row-1", buttons: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, "-", "+", "Delete"] },
-  {
-    id: "row-2",
-    buttons: ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "[", "]"],
-  },
-  {
-    id: "row-3",
-    buttons: ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "|"],
-  },
-  { id: "row-4", buttons: ["a", "s", "d", "f", "g", "h", "j", "k", "l", ":"] },
-  { id: "row-5", buttons: ["z", "x", "c", "v", "b", "n", "m", "Shift"] },
-  { id: "row-6 space", buttons: ["Space"] },
-];
-
 const keyboard = document.querySelector(".keyboard");
 const textArea = document.getElementById("textarea");
 const audioElement = document.getElementById(`audio`);
 let isShifted = false;
 let displayInput = "";
+const markup = generateKeys();
+
+//generate keys
+function generateKeys() {
+  const keys = [
+    {
+      id: "row-1",
+      buttons: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, "-", "+", "Delete"],
+    },
+    {
+      id: "row-2",
+      buttons: ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "[", "]"],
+    },
+    {
+      id: "row-3",
+      buttons: ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "|"],
+    },
+    {
+      id: "row-4",
+      buttons: ["a", "s", "d", "f", "g", "h", "j", "k", "l", ":"],
+    },
+    { id: "row-5", buttons: ["z", "x", "c", "v", "b", "n", "m", "Shift"] },
+    { id: "row-6 space", buttons: ["Space"] },
+  ];
+  return `${keys
+    .map(
+      (row) =>
+        `<div class='row' id=${row.id}>${row.buttons
+          .map((key) => `<button class='key'>${key}</button>`)
+          .join("")}</div>`,
+    )
+    .join("")}`;
+}
 
 // Play audio function
 const playAudio = (audioElement) => {
@@ -28,15 +46,6 @@ const playAudio = (audioElement) => {
     audioElement.play();
   }
 };
-
-const markup = `${keys
-  .map(
-    (row) =>
-      `<div class='row' id=${row.id}>${row.buttons
-        .map((key) => `<button class='key'>${key}</button>`)
-        .join("")}</div>`,
-  )
-  .join("")}`;
 
 const showInput = (input) => {
   textArea.value = input;
